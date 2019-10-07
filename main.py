de188b3c2a05c8699cb13c2015d7434f6a8bc1eb
psychobuddha <psychobuddha@mailfence.com>
2019-10-08 00:24:49 +0200

    Improved output

--- main.py
+++ main.py
@@ -1,22 +1,29 @@
 import json
 from difflib import get_close_matches
-data = json.load(open('data.json'))
+data = json.load(open('/home/ninja/Desktop/DictionaryProject/Dictionary/data.json'))
 
-compare = get_close_matches(phrase, data.keys()
+phrase = input('Enter word: ')
 
 def translate(word):
-    
+    word = word.lower()
+    diff_catcher = get_close_matches(phrase, data.keys())
 
     if word in data:
         return data[word]
-    elif len(get_close_matches(phrase, data.keys())) > 0:
-        ys = input(f'Did you mean "{get_close_matches(phrase, data.keys())[0]} instead"? Enter Y/N\n')
-        if ys == 'Y' or ys == 'yes':
-            return data[get_close_matches(phrase, data.keys())[0]]
-        return 'Word not in dictionary'
+    elif len(diff_catcher) > 0:
+        ys = input(f'Did you mean "{diff_catcher[0]} instead"? Enter Y/N\n')
+        if ys == 'Y':
+            return data[diff_catcher[0]]
+        else:
+            return f'{phrase} not in dictionary'
     else:   
         return 'Word not in dictionary'
 
-phrase = input('Enter word: ').lower()
 
-print(translate(phrase))
+output = translate(phrase)
+
+if type(output) == list:
+    for item in output:
+        print(item)
+else:
+    print(output)
